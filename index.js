9ac7e56ee5cf2d01026863b3a21da8b708b09f5a
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-21 13:30:51 +0530

    feat - input-output-processing

---
+++ index.js
@@ -0,0 +1,23 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const errorMessage = document.querySelector("#error-message");
+const checkButton = document.querySelector("#check-btn");
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  errorMessage.style.display = "none";
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+
+    } else {
+      showMessage("The cash should be at least equal to bill amount");
+    }
+  } else {
+    showMessage("The bill amount should be greater than 0");
+  }
+});
+
+function showMessage(message) {
+  errorMessage.style.display = "block";
+  errorMessage.innerText = message;
+}
+
