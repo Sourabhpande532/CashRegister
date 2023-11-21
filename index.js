861c6e05ed2e8f9b583c1821605658389cd9e009
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-21 20:22:46 +0530

    refactor - code

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const errorMessage = document.querySelector("#error-message");
 const checkButton = document.querySelector("#check-btn");
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
-  errorMessage.style.display = "none";
+  hideMessage();
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
-
+      const amountToBeReturn = cashGiven.value - billAmount.value;
+      calculateChange(amountToBeReturn);
     } else {
-      showMessage("The cash you provided should be at least equal to bill amount");
+      showMessage(
+        "The cash you provided should be at least equal to bill amount"
+      );
     }
   } else {
     showMessage("The bill amount should be greater than 0");
   }
 });
 
+function hideMessage() {
+errorMessage.style.display = "none";
+}
+
 function showMessage(message) {
-  errorMessage.style.display = "block";
-  errorMessage.innerText = message;
+errorMessage.style.display = "block";
+ errorMessage.innerText = message;
 }
 
