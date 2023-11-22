2c9d2784a2773a7ac65a0fb5b3ae5a765e577892
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-22 14:53:53 +0530

    refactor - build logic

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cashGiven = document.querySelector("#cash-given");
 const errorMessage = document.querySelector("#error-message");
 const checkButton = document.querySelector("#check-btn");
 
+const noOfNotesAvailable = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000,500,100,20,10,5,1]
+
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
   if (billAmount.value > 0) {
@@ -11,13 +15,24 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
       calculateChange(amountToBeReturn);
     } else {
       showMessage(
-        "The cash you provided should be at least equal to bill amount"
+        "Do you wanna wash dishes? 🍽️"
       );
     }
   } else {
-    showMessage("The bill amount should be greater than 0");
+    showMessage("Invalid Bill Amount 😏");
   }
 });
+
+function calculateChange(amountToBeReturned){
+for(let i = 0; i < availableNotes.length; i++){
+  /* No. of Notes need for the denomination */
+  const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+  /* Then we need to update the amount to be return remaining one value after*/
+  amountToBeReturned = amountToBeReturned % availableNotes[i];
+  /* update the no. of Notes in the table for the current ammount */
+  noOfNotesAvailable[i].innerText = numberOfNotes;
+}
+}
 
 function hideMessage() {
 errorMessage.style.display = "none";
