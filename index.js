25aae989248411e96f73fbef17aa8d70467b0bfb
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-22 15:19:51 +0530

    refactor & modify - [logic]

--- index.js
+++ index.js
@@ -5,41 +5,38 @@ const checkButton = document.querySelector("#check-btn");
 
 const noOfNotesAvailable = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000,500,100,20,10,5,1]
+const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
-  hideMessage();
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturn = cashGiven.value - billAmount.value;
-      calculateChange(amountToBeReturn);
+    hideMessage();
+    if (Number(billAmount.value) > 0) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+            const amountToBeReturn = Number(cashGiven.value) - Number(billAmount.value);
+            calculateChange(amountToBeReturn);
+        } else {
+            showMessage("Do you wanna wash dishes? 🍽️");
+        }
     } else {
-      showMessage(
-        "Do you wanna wash dishes? 🍽️"
-      );
+        showMessage("Invalid Bill Amount 😏");
     }
-  } else {
-    showMessage("Invalid Bill Amount 😏");
-  }
 });
 
-function calculateChange(amountToBeReturned){
-for(let i = 0; i < availableNotes.length; i++){
-  /* No. of Notes need for the denomination */
-  const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-  /* Then we need to update the amount to be return remaining one value after*/
-  amountToBeReturned = amountToBeReturned % availableNotes[i];
-  /* update the no. of Notes in the table for the current ammount */
-  noOfNotesAvailable[i].innerText = numberOfNotes;
-}
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+         /* No. of Notes need for the denomination */
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+         /* Then we need to update the amount to be return remaining value after denomination*/
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+         /* update the no. of Notes in the table for the current ammount */
+        noOfNotesAvailable[i].innerText = numberOfNotes;
+    }
 }
 
 function hideMessage() {
-errorMessage.style.display = "none";
+    errorMessage.style.display = "none";
 }
 
 function showMessage(message) {
-errorMessage.style.display = "block";
- errorMessage.innerText = message;
+    errorMessage.style.display = "block";
+    errorMessage.innerText = message;
 }
-
